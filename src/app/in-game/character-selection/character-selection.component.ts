ab286891bfc8ed2d8214a6fd130eaf27aaeb6d51
pecodigos <pedrenriquecunha@gmail.com>
2024-11-27 16:02:52 -0300

    Starting to make character-selection page

--- src/app/in-game/character-selection/character-selection.component.ts
+++ src/app/in-game/character-selection/character-selection.component.ts
@@ -1,15 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-character-selection',
   standalone: true,
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    CommonModule
   ],
   templateUrl: './character-selection.component.html',
   styleUrl: './character-selection.component.scss'
 })
 export class CharacterSelectionComponent {
+  characters = Array.from({ length: 60 }, (_, i) => ({
+    id: i + 1,
+    name: `Character ${i + 1}`,
+    image: `../../../assets/GokuIcon.png`
+  }));
 
+  currentPage = 0;
+  charactersPerPage = 20;
+
+  get visibleCharacters() {
+    const startIndex = this.currentPage * this.charactersPerPage;
+    const endIndex = startIndex + this.charactersPerPage;
+    return this.characters.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    if ((this.currentPage + 1) * this.charactersPerPage < this.characters.length) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
 }
