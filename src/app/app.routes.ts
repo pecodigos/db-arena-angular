8f26458981fdb0456dd8188cb95b8efc4e576c77
pecodigos <pedrenriquecunha@gmail.com>
2024-11-27 00:46:03 -0300

    adding lots of routes and favicon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { GameManualComponent } from './game-manual/game-manual.component';
 import { HomeComponent } from './home/home.component';
 import { MissionsComponent } from './missions/missions.component';
 import { InGameComponent } from './in-game/in-game.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +13,7 @@ export const routes: Routes = [
   { path: 'game-manual', component: GameManualComponent },
   { path: 'missions', component: MissionsComponent },
   { path: 'in-game', component: InGameComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'leaderboards', component: LeaderboardComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
