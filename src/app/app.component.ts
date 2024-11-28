b94d244c969f3bb5f405853a31ba09b3940f1581
pecodigos <pedrenriquecunha@gmail.com>
2024-11-28 16:22:34 -0300

    toggle button and minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,10 +15,21 @@ import { CommonModule } from '@angular/common';
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
+    MatButtonToggleModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, private renderer: Renderer2) {}
+
+  isDarkMode = false;
+
+  toggleTheme(isDarkMode: boolean) {
+    this.isDarkMode = isDarkMode;
+
+    const themeClass = isDarkMode ? 'dark-theme ' : 'light-theme';
+    this.renderer.removeClass(document.body, isDarkMode ? 'light-theme' : 'dark-theme');
+    this.renderer.addClass(document.body, themeClass);
+  }
 }
